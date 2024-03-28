48463969b5719dd75d12605dca05b7d78fdb69ba
IzacLuthfi <165124150+IzacLuthfi@users.noreply.github.com>
2024-03-28 16:59:52 +0700

    Update peminjaman_kamera.py

---
+++ peminjaman_kamera.py
@@ -0,0 +1,12 @@
+from method import Metode
+
+print("Selamat datang di Program Peminjaman Kamera\n")
+
+def listKamera():
+    print("""Daftar kamera yang tersedia:
+1 : Canon EOS R       - Rp. 200.000
+2 : Sony Alpha A7 III - Rp. 150.000
+3 : Nikon Z6          - Rp. 170.000
+4 : Fujifilm X-T4     - Rp. 140.000
+5 : Panasonic Lumix GH5 - Rp. 160.000
+0 : Batal""")
