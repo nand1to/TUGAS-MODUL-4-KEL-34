9b3809384c9f09f3b2892a95026fe0daf45d5220
mhmdabdulmajid19 <150311523+mhmdabdulmajid19@users.noreply.github.com>
2024-03-28 16:52:41 +0700

    Add files via upload

---
+++ method.py
@@ -0,0 +1,8 @@
+class Metode:
+    def __init__(self, total_harga):
+        self.total_harga = total_harga
+
+    def trims(self):
+        print("Terima kasih telah menggunakan program ini.")
+
+  
