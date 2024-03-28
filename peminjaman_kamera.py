f763118d5f4df726f602f20af2ee0075fe476b64
nand1to <165172601+nand1to@users.noreply.github.com>
2024-03-28 03:23:08 -0700

    Update nandito peminjaman_kamera.py

--- peminjaman_kamera.py
+++ peminjaman_kamera.py
@@ -10,3 +10,24 @@ def listKamera():
 4 : Fujifilm X-T4     - Rp. 140.000
 5 : Panasonic Lumix GH5 - Rp. 160.000
 0 : Batal""")
+    
+def opsiKamera(opsi):
+    switcher = {
+        1: "Canon EOS R",
+        2: "Sony Alpha A7 III",
+        3: "Nikon Z6",
+        4: "Fujifilm X-T4",
+        5: "Panasonic Lumix GH5",
+        0: "Keluar dari program",
+    }
+    return switcher.get(opsi, "Pilihan tidak valid")
+
+def hargaKamera(opsi):
+    switcher = {
+        1: 200000,
+        2: 150000,
+        3: 170000,
+        4: 140000,
+        5: 160000,
+    }
+    return switcher.get(opsi, 0)
